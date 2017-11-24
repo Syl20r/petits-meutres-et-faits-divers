14b0d8456f8ca7392c21504c120b5dc29ca31ecd
Sylvain <sylvainreynaud@outlook.com>
2017-11-24 16:29:05 +0100

    123

--- client/js/client.js
+++ client/js/client.js
@@ -1,6 +1,9 @@
+var sock = io();
+
+// Reception d'un msg
+sock.on('msg', onConnection);
+
 function onConnection(text) {
   var el = document.getElementById('connexion');
   el.innerHTML = text;
-}
-
-onConnection("Connecté !");
+}125874
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 'use strict';
 let http = require('http');
 let express = require('express');
+let socketio = require('socket.io');
+
 // Application
 let app = express();
 let server = http.createServer(app);
+let io = socketio(server);
+
+// Reception d'une connexion alors on execute la fonction ...
+io.on('connection', () => sock.emit('msg', 'Hello!'));
 
 // Le navigateur ouvre directement le dossier 'client'
 app.use(express.static(__dirname + '/client'));
