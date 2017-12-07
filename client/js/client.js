d2e911a4fc7dccee068803557c74a4da09af5603
Jérémie Roux <jeremie.roux@etu.umontpellier.fr>
2017-12-07 09:58:31 +0100

    mise en page et box size

--- client/js/client.js
+++ client/js/client.js
@@ -111,3 +111,17 @@ if (document.cookie.length>0)
   }
 return "";
 }
+
+function hauteur()
+{
+    var divHeight;
+    var obj = document.getElementById('container');
+
+    if(obj.offsetHeight)          {divHeight=obj.offsetHeight;}
+    else if(obj.style.pixelHeight){divHeight=obj.style.pixelHeight;}
+
+    var obj2 = document.getElementById('aside');
+    var obj3 = document.getElementById('content');
+    obj2.style.height = divHeight+"px";
+    obj3.style.height = divHeight+"px";
+}
